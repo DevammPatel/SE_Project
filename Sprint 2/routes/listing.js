7ea1affa8adcf473a389e99b90cb75483cde1862
DevammPatel <122225364+DevammPatel@users.noreply.github.com>
2024-11-10 21:48:55 +0530

    Delete Sprint 2 directory

--- Sprint 2/routes/listing.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const listingController = require('../controllers/listingController');
-
-// Route to search and filter NGOs
-router.get('/search', listingController.searchNGOs);
-
-module.exports = router;
--- Sprint 2/routes/profile.js
+++
@@ -1,8 +0,0 @@
-// routes/profileRoutes.js
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-
-router.put('/update/:ngoId', profileController.updateNGOProfile);
-
-module.exports = router;
